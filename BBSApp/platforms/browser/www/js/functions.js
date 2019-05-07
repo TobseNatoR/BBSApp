21b7bd508e95b9b2bbf3c0670f0577cd1fcfc42b
TobseNatoR <42202420+TobseNatoR@users.noreply.github.com>
2019-05-07 13:58:59 +0200

    001

--- BBSApp/platforms/browser/www/js/functions.js
+++ BBSApp/platforms/browser/www/js/functions.js
@@ -1,5 +1,16 @@
 ﻿function initJava()
 {
     //Erst alle App Daten laden lassen
+}
 
+function loadAppData()
+{
+    $.getJSON("https://filebase.strawberry-rp.de/app/authenticationController.php", function (result) {
+        $.each(result, function (i, field) {
+            if (field.Modell == device.model && field.UUID == device.uuid && field.Serial == device.serial)
+            {
+                
+            }
+        });
+    });
 }
