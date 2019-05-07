d8731a9f14371f82b5e9b10ede95f07841d11d6f
Torben Klaener <41986643+torben06@users.noreply.github.com>
2019-05-07 14:36:04 +0200

    Basis

---
+++ BBSApp/platforms/android/app/src/main/assets/www/js/Controller.js
@@ -0,0 +1,40 @@
+﻿var registered = false;
+
+
+function initJava()
+{
+    loadAppData();
+}
+
+function loadAppData()
+{
+    var req = $.getJSON("https://filebase.strawberry-rp.de/app/authenticationController.php", function (result)
+    {
+        $.each(result, function (i, field)
+        {
+            if (field.model == device.model && field.uuid == device.uuid && field.serial == device.serial)
+            {         
+                registered = true;
+            }
+        });
+    });
+
+    req.success(function (response)
+    {
+        if (registered == false)
+        {
+            window.location.href = "#register";
+        }
+
+        console.log(device.serial);
+        console.log(device.uuid);
+        console.log(device.model);
+
+        console.log("fertig");
+    });
+}
+
+function registerUser()
+{
+
+}
