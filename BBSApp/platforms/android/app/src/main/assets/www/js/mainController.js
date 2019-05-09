60374b5da1662863e53b4ee61df88f021f3b490c
TobseNatoR <42202420+TobseNatoR@users.noreply.github.com>
2019-05-09 12:54:58 +0200

    commit

--- BBSApp/platforms/android/app/src/main/assets/www/js/mainController.js
+++ BBSApp/platforms/android/app/src/main/assets/www/js/mainController.js
@@ -8,7 +8,7 @@ function initJava()
 
 function loadAppData()
 {
-    var req = $.getJSON("https://filebase.strawberry-rp.de/app/authenticationController.php", function (result)
+    var req = $.getJSON("https://filebase.strawberry-rp.de/app/get/authenticationController.php", function (result)
     {
         $.each(result, function (i, field)
         {
@@ -30,9 +30,19 @@ function loadAppData()
     });
 }
 
-function registerUser()
+function registerUser(email, username, password, password1)
 {
+    var str = "?model=" + device.model + "&serial=" + device.serial + "&uuid=" + device.uuid + "&email=" + email + "&username=" + username + "&password=" + password;
+    var url = "https://filebase.strawberry-rp.de/app/send/authenticationController.php" + str;
+    xmlReq = new XMLHttpRequest();
+    xmlReq.open("POST", url, true);
+    xmlReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlReq.setRequestHeader("Content-length", str.length);
+    xmlReq.setRequestHeader("Connection", "close");
+    xmlReq.send(str);
 
+    registered = true;
+    window.location.href = "#startseite";
 }
 
 (function ()
@@ -45,9 +55,9 @@ function registerUser()
     function onDeviceReady()
     {
 
-        $("#register").click(function ()
+        $("#registerbutton").click(function ()
         {
-            alert("geht");
+            registerUser(document.getElementById('email').value, document.getElementById('nickname').value, document.getElementById('password').value, document.getElementById('password1').value)
         });
 
     };
