70dd4ef0a2479bb4c50693bd626938d7551370bd
TobseNatoR <42202420+TobseNatoR@users.noreply.github.com>
2019-06-04 14:03:23 +0200

    hallo!

--- BBSApp/www/js/mainController.js
+++ BBSApp/www/js/mainController.js
@@ -1,25 +1,31 @@
-﻿var registered = false;
+﻿//Globale variablen
+var registered = false;
 
-
+//App wird initialisiert
 function initJava()
 {
     loadAppData();
 }
 
+//App Daten werden aus der MySQL Datenbank abgerufen
 function loadAppData()
 {
+    //JSON Request
     var req = $.getJSON("https://filebase.strawberry-rp.de/app/get/authenticationController.php", function (result)
     {
         $.each(result, function (i, field)
         {
             if (field.model == device.model && field.uuid == device.uuid && field.serial == device.serial)
             {
+                //Lokal wird dei Eindeutige ID aus der Datenbank gespeichert
                 window.localStorage.setItem('id', field.id);
                 registered = true;
             }
         });
     });
 
+    //Wenn der Request fertig ist wird gefragt ob der User ín der Datenbank vorhanden ist
+    //falls ja passiert nichts und falls nein kommt der Nutzer in das registrierungsformular
     req.success(function (response)
     {
         if (registered == false)
@@ -31,6 +37,7 @@ function loadAppData()
     });
 }
 
+//Hier wird ein HTTP Request zum Server gesendet welcher die nötigen Geräteinformationen etc. überträgt
 function registerUser(email, username, password, password1)
 {
     var str = "?model=" + device.model + "&serial=" + device.serial + "&uuid=" + device.uuid + "&email=" + email + "&username=" + username + "&password=" + password;
@@ -56,8 +63,10 @@ function registerUser(email, username, password, password1)
     function onDeviceReady()
     {
 
+        //Der Button für das Registrierungsformular
         $("#registerbutton").click(function ()
         {
+            //Funktion wird gecallt
             registerUser(document.getElementById('email').value, document.getElementById('nickname').value, document.getElementById('password').value, document.getElementById('password1').value)
         });
 
